38af65728a6a3e46015fe10ad0e970e945beb89c
jmacboy <j.m.alvarez.camacho@gmail.com>
2022-07-16 11:14:17 -0400

    formateo de archivo

--- Pedidos.Application/UseCases/Command/Pedidos/CrearPedido/CrearPedidoHandler.cs
+++ Pedidos.Application/UseCases/Command/Pedidos/CrearPedido/CrearPedidoHandler.cs
@@ -8,22 +8,19 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Pedidos.Application.UseCases.Command.Pedidos.CrearPedido
-{
-    public class CrearPedidoHandler : IRequestHandler<CrearPedidoCommand, Guid>
-    {
+namespace Pedidos.Application.UseCases.Command.Pedidos.CrearPedido {
+    public class CrearPedidoHandler : IRequestHandler<CrearPedidoCommand, Guid> {
         private readonly IPedidoRepository _pedidoRepository;
         private readonly ILogger<CrearPedidoHandler> _logger;
         private readonly IPedidoService _pedidoService;
         private readonly IPedidoFactory _pedidoFactory;
         private readonly IUnitOfWork _unitOfWork;
 
-        public CrearPedidoHandler(IPedidoRepository pedidoRepository, 
+        public CrearPedidoHandler(IPedidoRepository pedidoRepository,
             ILogger<CrearPedidoHandler> logger,
-            IPedidoService pedidoService, 
-            IPedidoFactory pedidoFactory, 
-            IUnitOfWork unitOfWork)
-        {
+            IPedidoService pedidoService,
+            IPedidoFactory pedidoFactory,
+            IUnitOfWork unitOfWork) {
             _pedidoRepository = pedidoRepository;
             _logger = logger;
             _pedidoService = pedidoService;
@@ -31,15 +28,12 @@ namespace Pedidos.Application.UseCases.Command.Pedidos.CrearPedido
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<Guid> Handle(CrearPedidoCommand request, CancellationToken cancellationToken)
-        {
-            try
-            {
+        public async Task<Guid> Handle(CrearPedidoCommand request, CancellationToken cancellationToken) {
+            try {
                 string nroPedido = await _pedidoService.GenerarNroPedidoAsync();
                 Pedido objPedido = _pedidoFactory.Create(nroPedido);
 
-                foreach (var item in request.Detalle)
-                {
+                foreach (var item in request.Detalle) {
                     objPedido.AgregarItem(item.ProductoId, item.Cantidad, item.Precio, item.Instrucciones);
                 }
 
@@ -51,9 +45,7 @@ namespace Pedidos.Application.UseCases.Command.Pedidos.CrearPedido
                 await _unitOfWork.Commit();
 
                 return objPedido.Id;
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 _logger.LogError(ex, "Error al crear pedido");
             }
             return Guid.Empty;
